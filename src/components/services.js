23e58ecb7a14364e961692ac19452734b474db58
Manvitha <116058038+manvithaaa04@users.noreply.github.com>
2024-06-01 21:52:08 +0530

    Update services.js

--- src/components/services.js
+++ src/components/services.js
@@ -5,7 +5,7 @@ function Services() {
   return (
     <div className="container">
       <div className="header">
-        <h2>Wemofy, Where Design, Development, and Machine Learning converge to create transformative solutions for businesses.</h2>
+        <h2> Where Design, Development, and Machine Learning converge to create transformative solutions for businesses.</h2>
         <p>We are a team of top talent delivering enterprise solutions globally. We evolve with the advancement in technology because we believe in making our technology as your innovation.</p>
       </div>
           </div>
